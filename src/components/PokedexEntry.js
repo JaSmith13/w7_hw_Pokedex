748cd056fce6486f464eeb1b725ed3c43008060e
Alastair Smith <j.alastair.smith@outlook.com>
2021-06-26 19:19:20 +0100

    adds height weight and type to info

--- src/components/PokedexEntry.js
+++ src/components/PokedexEntry.js
@@ -1,10 +1,22 @@
 const PokedexEntry = ({selectedPokemon}) => {
 
     const pokemon = selectedPokemon
+
+    const findTypes = () => {
+        return pokemon.types.map(type => {
+            return <li>{type.type.name}</li>
+        })
+    }
+
     return(
         <section>
             <img src={pokemon.sprites.front_default} alt="selected pokemon" />
-            <p>{pokemon.name}</p>
+            <h4>{pokemon.name}</h4>
+            <p>Height: {(pokemon.height)/10}m</p>
+            <p>Weight: {(pokemon.weight/10)}kg</p>
+            <ul>
+                Type: {findTypes()} 
+            </ul>
         </section>
     )
 }
